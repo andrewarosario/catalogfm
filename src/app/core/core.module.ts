1b4a4e5159c1e01dc5450b200ba897ff89275e8d
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-09 13:49:24 -0300

    exportando módulos principais no CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
