af3158901e3a5fcfc1f3196df228f29618150d0e
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-25 13:51:33 -0300

    refatorando auth guard

--- src/app/core/lastfm/guards/lastfm-auth.guard.ts
+++ src/app/core/lastfm/guards/lastfm-auth.guard.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { LastfmAuthService } from '../services/lastfm-auth.service';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { tap, catchError, map, mapTo } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ export class LastfmAuthGuard implements CanActivate {
 
   constructor(
     private router: Router,
-    private localStorageService: LocalStorageService,
-    private lastfmAuthService: LastfmAuthService
+    private authService: AuthService,
+    private localStorageService: LocalStorageService
   ) {
 
   }
@@ -22,17 +23,21 @@ export class LastfmAuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    const token = this.localStorageService.getKey('x-access-token');
+    const token = this.getToken(next);
     if (!token) {
       this.router.navigate(['/auth']);
       return false;
     }
 
-    return this.lastfmAuthService.authenticate(token)
+    return this.authService.authenticate(token)
       .pipe(
         tap(res => console.log({res})),
         mapTo(true)
       );
+  }
+
+  private getToken(next: ActivatedRouteSnapshot): string {
+    return this.localStorageService.getKey('key') || next.queryParams.token;
   }
 
 }
