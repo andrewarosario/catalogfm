42d90d8597915269816d4f572331494a06a8d90a
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-26 06:59:37 -0300

    criando ScrobbleBulkComponent

---
+++ src/app/scrobble/scrobble-bulk/containers/scrobble-bulk/scrobble-bulk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scrobble-bulk',
+  templateUrl: './scrobble-bulk.component.html',
+  styleUrls: ['./scrobble-bulk.component.scss']
+})
+export class ScrobbleBulkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
