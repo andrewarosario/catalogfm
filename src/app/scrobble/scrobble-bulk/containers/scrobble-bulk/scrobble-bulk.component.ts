7ea5aa56b5d08051f0159a9a96b592dcbacfbf91
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-27 07:02:58 -0300

    criando lógica do Bulk Scrobble

--- src/app/scrobble/scrobble-bulk/containers/scrobble-bulk/scrobble-bulk.component.ts
+++ src/app/scrobble/scrobble-bulk/containers/scrobble-bulk/scrobble-bulk.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ScrobbleBulkFacade } from '../../scrobble-bulk.facade';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-scrobble-bulk',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScrobbleBulkComponent implements OnInit {
 
-  constructor() { }
+  public scrobbleForm: FormGroup;
+  public loadingRequest = false;
+
+  constructor(
+    public facade: ScrobbleBulkFacade,
+    private formBuilder: FormBuilder,
+    private matSnackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.scrobbleForm = this.formBuilder.group({
+      textScrobble: ['', Validators.required]
+    });
+  }
+
+  scrobble() {
+    this.loadingRequest = true;
+    const textScrobble = this.scrobbleForm.get('textScrobble').value;
+    this.facade.scrobble(textScrobble).subscribe(
+      () => this.matSnackBar.open('Faixas scrobbladas com sucesso!', 'Ok', { duration: 3000, verticalPosition: 'top'}),
+      err => this.matSnackBar.open('Erro ao scrobblar as faixas!', 'Ok', { duration: 3000, verticalPosition: 'top'}),
+      () => this.loadingRequest = false
+    );
   }
 
 }
