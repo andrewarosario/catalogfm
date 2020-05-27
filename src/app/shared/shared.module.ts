13068373fea4151babb4ee9c93e3ae2350ef5365
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-27 07:01:12 -0300

    exportando componentes do Material no SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule
   ]
 })
 export class SharedModule { }
