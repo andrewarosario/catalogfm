47b511a1c50e7f9faa12237f151dc3feb25c7584
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-30 11:55:04 -0300

    criando estratégia genérica do IndexedDb

---
+++ src/app/core/indexed-db/indexed-db-table.ts
@@ -0,0 +1,36 @@
+import { Injector } from '@angular/core';
+import { from } from 'rxjs';
+import { IndexedDbService } from './indexed-db.service';
+import { uuid } from 'src/app/shared/helpers/uuid';
+
+export class IndexedDbTable<T extends {id?: string}> {
+
+  protected table: Dexie.Table<T, string>;
+  protected db: IndexedDbService;
+
+  constructor(
+    protected injector: Injector,
+    tableName: string
+  ) {
+    this.db = injector.get(IndexedDbService);
+    this.createTable(tableName);
+  }
+
+  public add(data: T) {
+    const dbData = { id: uuid(), ...data};
+    return from(this.table.add(dbData));
+  }
+
+  public delete(id: string) {
+    return from(this.table.delete(id));
+  }
+
+  public getAll() {
+    return from(this.table.toArray());
+  }
+
+  private createTable(tableName: string): void {
+    this.db.createTable(tableName);
+    this.table = this.db.table(tableName);
+  }
+}
